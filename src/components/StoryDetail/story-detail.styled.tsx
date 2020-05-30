df7077ff00db787d6434f3256330700c821a9cb1
Lucas Santos <lucazwebmail@gmail.com>
2020-05-29 22:19:43 -0300

    style: add mobile rules to story detail modal

--- src/components/StoryDetail/story-detail.styled.tsx
+++ src/components/StoryDetail/story-detail.styled.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Colors } from '../Config/constants';
+import { Breakpoint, Colors } from '../Config/constants';
 import { H2 } from '../Config/typography.component';
 import { StyledImageBox } from '../Story/story.styled';
 
@@ -20,9 +20,14 @@ export const StoryModal = styled.div`
   width: 50%;
   height: 100vh;
   background: white;
-  border-left: 10px solid ${Colors.primary};
+  border-left: 4px solid ${Colors.primary};
   padding: 32px;
   overflow-y: scroll;
+
+  @media (max-width: ${Breakpoint.sm}em) {
+    width: 100%;
+    padding: 16px;
+  }
 `;
 
 export const CloseBtn = styled.button`
@@ -52,11 +57,19 @@ export const StoryTitle = styled(H2)`
   width: 90%;
   font-size: 1.5em;
   margin-bottom: 32px;
+  @media (max-width: ${Breakpoint.sm}em) {
+    font-size: 1.2em;
+    width: 80%;
+  }
 `;
 
 export const StoryFeaturedImage = styled(StyledImageBox)`
   width: 100%;
   height: 350px;
+  filter: none;
+  @media (max-width: ${Breakpoint.sm}em) {
+    height: 200px;
+  }
 `;
 
 export const StoryContent = styled.p`
